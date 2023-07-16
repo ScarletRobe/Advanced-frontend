618bbb9cfe6705b01ef42283816d1b81db86c574
Leonid Chernyakov <scrltrb@gmail.com>
2023-07-17 00:08:53 +0300

    React JSX, SASS loader

--- config/build/helpers/buildLoaders.ts
+++ config/build/helpers/buildLoaders.ts
@@ -2,13 +2,23 @@ import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
 
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
+  const styleLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      "style-loader",
+      "css-loader",
+      "sass-loader",
+    ],
+  };
 
-    return [
-        typescriptLoader,
-    ]
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  }
+
+  return [
+    typescriptLoader,
+    styleLoader
+  ]
 }
