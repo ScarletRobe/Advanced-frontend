abf368f307d8dda9c2651f9d2094b5d59e76714d
Leonid Chernyakov <scrltrb@gmail.com>
2023-07-17 23:45:16 +0300

    Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { Suspense, useContext } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import Counter from "./components/Counter";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 
-import './index.scss'
-import { Suspense } from "react";
+import './styles/index.scss'
+import { ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+  const {theme,toggleTheme} = useTheme();
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Сменить тему</button>
       <Link to="/">Главная</Link>
       <Link to="/about">О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
