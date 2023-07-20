367da9099609264d4460bb677f6043a2a358372f
Leonid Chernyakov <scrltrb@gmail.com>
2023-07-20 20:21:40 +0300

    Decompose routes config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,6 +7,7 @@ import { AboutPage } from "pages/AboutPage";
 import { MainPage } from "pages/MainPage";
 
 import './styles/index.scss'
+import { AppRouter } from "./providers/router";
 
 const App = () => {
   const {theme,toggleTheme} = useTheme();
@@ -15,12 +16,7 @@ const App = () => {
       <button onClick={toggleTheme}>Сменить тему</button>
       <Link to="/">Главная</Link>
       <Link to="/about">О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<MainPage />}/>
-          <Route path="/about" element={<AboutPage />}/>
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,18 @@
+import React, { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import { routeConfig } from "shared/config/routeConfig/routeConfig";
+
+const AppRouter = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        {Object.values(routeConfig).map((config) => <Route {...config} />)}
+      </Routes>
+    </Suspense>
+  );
+};
+
+export default AppRouter;
