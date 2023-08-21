2b8a3ef7ee5952af6ab8a758539ee619969f3e47
Leonid Chernyakov <scrltrb@gmail.com>
2023-08-21 21:17:41 +0300

    Decorators for storybook

---
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -0,0 +1,10 @@
+import { StoryFn } from "@storybook/react";
+import { Theme } from "app/providers/ThemeProvider";
+
+export const ThemeDecorator = (theme: Theme) => (Story: StoryFn) => {
+  return (
+    <div style={{width: '100dvw'}} className={`app ${theme}`}>
+      <Story />
+    </div>
+  )
+}
